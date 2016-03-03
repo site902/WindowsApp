e33aa59f9c635acd7a3e0cf73529a213baf6959e
ororor2000 <ororor2000@gmail.com>
2016-03-03 16:32:56 +0200

    ddd

---
+++ Register.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Register : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        List<KeyValuePair<string, string>> fields = new List<KeyValuePair<string, string>>();
+
+        string[] keys;
+    }
+}
